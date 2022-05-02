fb7f8a9dc2423e81acd1dc94c6cd7f87a24f35b2
mrbid <james@voxdsp.com>
2022-05-02 09:47:49 +0100

    general

--- multitrain/cat.sh
+++ multitrain/cat.sh
@@ -1,2 +1,2 @@
-cat d2/dataset_x.dat d3/dataset_x.dat d4/dataset_x.dat d5/dataset_x.dat d6/dataset_x.dat d7/dataset_x.dat d8/dataset_x.dat d9/dataset_x.dat d10/dataset_x.dat > dataset_x.dat
+cat d1/dataset_x.dat d2/dataset_x.dat d3/dataset_x.dat d4/dataset_x.dat d5/dataset_x.dat d6/dataset_x.dat d7/dataset_x.dat d8/dataset_x.dat d9/dataset_x.dat d10/dataset_x.dat > dataset_x.dat
 cat d1/dataset_y.dat d2/dataset_y.dat d3/dataset_y.dat d4/dataset_y.dat d5/dataset_y.dat d6/dataset_y.dat d7/dataset_y.dat d8/dataset_y.dat d9/dataset_y.dat d10/dataset_y.dat > dataset_y.dat
