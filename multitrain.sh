2053b41d8d74dc8b09b863b351c1ebf8f1b672f9
mrbid <james@voxdsp.com>
2022-05-07 04:50:03 +0100

    general

--- multitrain.sh
+++ multitrain.sh
@@ -1,17 +1,27 @@
-#!/bin/bash
-layers=0
-units=384
-batches=32
-for i in {0..4}; do
+# #!/bin/bash
+# layers=0
+# units=384
+# batches=32
+# for i in {0..4}; do
 
-    optim="adam"
-    xterm -e "python3 train.py $layers $units $batches $optim 1" &
+#     optim="adam"
+#     xterm -e "python3 train.py $layers $units $batches $optim 1" &
 
-    optim="nesterov"
-    xterm -e "python3 train.py $layers $units $batches $optim 1" &
+#     optim="nesterov"
+#     xterm -e "python3 train.py $layers $units $batches $optim 1" &
 
-    echo "$layers $units $batches"
-    layers=$((layers+1))
-    read -p "Press any key for the next round of $layers layers"
+#     echo "$layers $units $batches"
+#     layers=$((layers+1))
+#     read -p "Press any key for the next round of $layers layers"
 
-done
+# done
+
+xterm -e "python3 train.py 4 384 32 nadam 1" &
+xterm -e "python3 train.py 4 384 32 adam 1" &
+xterm -e "python3 train.py 4 384 32 nesterov 1" &
+xterm -e "python3 train.py 4 384 32 sgd 1" &
+
+xterm -e "python3 train.py 4 768 32 nadam 1" &
+xterm -e "python3 train.py 4 768 32 adam 1" &
+xterm -e "python3 train.py 4 768 32 nesterov 1" &
+xterm -e "python3 train.py 4 768 32 sgd 1" &
