76e811d811004964801cdc54119185e13d5cfe1f
mrbid <james@voxdsp.com>
2022-05-09 16:20:53 +0100

    general

--- multitrain.sh
+++ multitrain.sh
@@ -39,11 +39,10 @@
 xterm -e "python3 train.py 4 869 32 nesterov 1" &
 
 xterm -e "python3 train2.py 16 32 32 tanh nesterov 1" &
-xterm -e "python3 train2.py 16 32 32 selu nesterov 1" &
-xterm -e "python3 train2.py 16 32 32 softsign nesterov 1" &
-xterm -e "python3 train2.py 16 32 32 relu nesterov 1" &
-
 xterm -e "python3 train2.py 32 64 32 tanh nesterov 1" &
-xterm -e "python3 train2.py 32 64 32 selu nesterov 1" &
-xterm -e "python3 train2.py 32 64 32 softsign nesterov 1" &
-xterm -e "python3 train2.py 32 64 32 relu nesterov 1" &
+xterm -e "python3 train2.py 64 128 32 tanh nesterov 1" &
+xterm -e "python3 train2.py 64 32 32 tanh nesterov 1" &
+xterm -e "python3 train2.py 128 32 32 tanh nesterov 1" &
+xterm -e "python3 train2.py 256 32 32 tanh nesterov 1" &
+xterm -e "python3 train2.py 256 64 32 tanh nesterov 1" &
+xterm -e "python3 train2.py 256 128 32 tanh nesterov 1" &
