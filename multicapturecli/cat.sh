20fb0ee56d15cf350daaf65f46f3ecd5a120cc66
mrbid <james@voxdsp.com>
2022-05-09 11:22:48 +0100

    general

--- multicapturecli/cat.sh
+++ multicapturecli/cat.sh
@@ -1,2 +1,2 @@
-cat 0.8_x.dat 0.9_x.dat > dataset_x.dat
-cat 0.8_y.dat 0.9_y.dat > dataset_y.dat
+cat 0.8_x.dat 0.9_x.dat 1.0_x.dat > dataset_x.dat
+cat 0.8_y.dat 0.9_y.dat 1.0_y.dat > dataset_y.dat
