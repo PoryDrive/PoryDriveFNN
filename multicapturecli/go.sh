998ed61d61fac14d8fa38b4f98664e45da4fe32c
mrbid <james@voxdsp.com>
2022-05-12 15:51:44 +0100

    general

--- multicapturecli/go.sh
+++ multicapturecli/go.sh
@@ -1,2 +1,2 @@
-for i in {1..512}; do nohup ./porydrivecli 32400 1200 0 > /dev/null 2>&1 & done
+for i in {1..512}; do nohup ./porydrivecli 32400 60 0 > /dev/null 2>&1 & done
 # for i in {1..1024}; do nohup ./porydrivecli 32400 32400 0.9 > /dev/null 2>&1 & done
